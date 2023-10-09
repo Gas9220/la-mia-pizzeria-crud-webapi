11dc9c4ed479a2776e6f92774507c3c97e608235
Gaspare <gasmo92@gmail.com>
2023-10-09 14:11:22 +0200

    GetAllPizzas Api

---
+++ la-mia-pizzeria-crud-mvc/Controllers/Api/PizzasController.cs
@@ -0,0 +1,25 @@
+﻿using la_mia_pizzeria_crud_mvc.Database;
+using la_mia_pizzeria_crud_mvc.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_crud_mvc.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        private PizzaContext _myDatabase;
+
+        public PizzasController(PizzaContext myDatabase)
+        {
+            _myDatabase = myDatabase;
+        }
+
+        public IActionResult GetAllPizzas()
+        {
+            IQueryable<Pizza> pizzas = _myDatabase.Pizzas;
+            return Ok(pizzas.ToList());
+        }
+    }
+}
